21bd7416be1ce0ed067aa4eda513ba68b9d0ad69
Fabio Bandacheski <fabio.versao.beta@gmail.com>
2020-07-30 23:08:14 -0300

    products create

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -13,10 +13,27 @@ interface IRequest {
 
 @injectable()
 class CreateProductService {
-  constructor(private productsRepository: IProductsRepository) {}
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
 
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
-    // TODO
+    const exist = await this.productsRepository.findByName(name);
+
+    console.log('product exist', exist);
+
+    if (exist) {
+      throw new AppError('Product exist!');
+    }
+
+    const product = await this.productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    return product;
   }
 }
 
